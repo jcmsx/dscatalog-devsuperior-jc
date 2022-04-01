32c7e5a2ec683f9cdcc859c4bf7d1a2e654a4fb1
Juliano Carlos de Oliveira <jcmsx@yahoo.com.br>
2022-04-01 20:28:21 -0300

    aula 1_16 postman

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,16 @@ public class CategoryService {
 	List<Category> list = repository.findAll();
 	return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 			
+	}
+
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		
+		Optional<Category> obj = repository.findById(id);
+		
+		Category entity = obj.get();
+		
+		return new CategoryDTO(entity);
 	}
 }
